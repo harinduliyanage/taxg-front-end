bb44e1e8ed27ee94ff7e41c1d48d5e3a5d2ce661
SachiniPiyumali <29sachi.piyumali@gmail.com>
2023-04-13 20:11:22 +0530

    Ui modifications knowledge

--- src/modules/knowledge/components/knowledge-view/index.jsx
+++ src/modules/knowledge/components/knowledge-view/index.jsx
@@ -1,7 +1,16 @@
 /* eslint-disable no-unused-vars */
 import { withTheme } from "@emotion/react";
 import "../../../../index.css";
-import { Box, Grid, Typography } from "@mui/material";
+import {
+  Box,
+  Grid,
+  Typography,
+  Button,
+  Avatar,
+  TextField,
+} from "@mui/material";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import KeyboardArrowUpOutlinedIcon from "@mui/icons-material/KeyboardArrowUpOutlined";
 import {
   AnswerCard1,
   NewQuestionCard,
@@ -11,8 +20,25 @@ import {
   ViewButton,
   PrimaryButton,
   SearchField,
+  ProvidersHeading,
+  ViewQuestionCard,
+  BottomCard,
+  RateCard,
+  OutLinedButton,
+  RateButton1,
+  QuestionIcon,
+  SourceCard,
+  LinkText,
+  PeopleGrid,
+  Person,
+  Frame1,
+  Content,
+  Name,
+  Title,
 } from "./style";
+import { ThumbsUp, ThumbsDown, ArrowDown } from "react-feather";
 import { People, Questions } from "./constants";
+import { useState } from "react";
 
 /**
  * Parent Knowledge view component define here.
@@ -21,6 +47,7 @@ import { People, Questions } from "./constants";
  */
 const KnowledgeView = () => {
   //
+  const [viewSources, setViewSources] = useState(false);
   return (
     <Grid
       container
@@ -44,54 +71,100 @@ const KnowledgeView = () => {
             flexDirection="column"
             key={index}
           >
-            <QuestionCard>{question.question}</QuestionCard>
-            <AnswerCard1>{question.answer}</AnswerCard1>
+            <QuestionCard>
+              <QuestionIcon src="./question.png" sx={{ mr: 2 }} />
+              <Typography className="question">{question.question}</Typography>
+            </QuestionCard>
+            <AnswerCard1 flexDirection="column">
+              <Typography className="view-answer">{question.answer}</Typography>
+              <BottomCard>
+                <RateCard>
+                  <RateButton1>
+                    <ThumbsUp />
+                  </RateButton1>
+                  <RateButton1>
+                    <ThumbsDown />
+                  </RateButton1>
+                </RateCard>
+                {!viewSources && (
+                  <Button
+                    className="outlined-button"
+                    variant="outlined"
+                    startIcon={<ArrowDropDownIcon />}
+                    onClick={() => setViewSources(true)}
+                  >
+                    View Resources
+                  </Button>
+                )}
+                {viewSources && (
+                  <Button
+                    className="outlined-button"
+                    variant="outlined"
+                    startIcon={<KeyboardArrowUpOutlinedIcon />}
+                    onClick={() => setViewSources(false)}
+                  >
+                    Hide Resources
+                  </Button>
+                )}
+              </BottomCard>
+              {viewSources && (
+                <SourceCard>
+                  <LinkText>
+                    https://s3.u.west-2.amazonaws.com/taxglobal-kendra/w-Elben.tsv
+                  </LinkText>
+                  <LinkText>
+                    https://s3.u.west-2.amazonaws.com/taxglobal-kendra/w-Elben.tsv
+                  </LinkText>
+                  <LinkText>
+                    https://s3.u.west-2.amazonaws.com/taxglobal-kendra/w-Elben.tsv
+                  </LinkText>
+                </SourceCard>
+              )}
+            </AnswerCard1>
           </Grid>
         ))}
-        <Grid
-          container
-          justifyContent="space-between"
-          position="relative"
-          alignItems="center"
-          flexDirection="column"
-        >
-          <NewQuestionCard>
-            <SearchField />
-          </NewQuestionCard>
-        </Grid>
+
+        <NewQuestionCard>
+          <Grid
+            className="search-field"
+            container
+            flexDirection="row"
+            alignItems="center"
+          >
+            <QuestionIcon
+              src="./question.png"
+              sx={{ mr: 2 }}
+              className="search-icon"
+            />
+            <TextField
+              className="search-placeholder"
+              placeholder="Ask follow up question"
+            />
+            <Grid className="background-focus" />
+          </Grid>
+          <ViewButton>Clear Conversation</ViewButton>
+        </NewQuestionCard>
       </Grid>
 
-      <Grid
-        container
-        item
-        xs={4}
-        justifyContent="space-between"
-        flexDirection="column"
-      >
+      <Grid container item xs={4} flexDirection="column">
         <SuggestedCard>
-          <Grid className="content">
-            <Typography className="suggested">
-              Matching service providers
-            </Typography>
-            <Grid className="people">
+          <Content>
+            <ProvidersHeading sx={{ fontFamily: "SF Pro Text" }}>
+              Matching Service Providers
+            </ProvidersHeading>
+            <PeopleGrid>
               {People?.map((person, index) => (
-                <Grid className="person" key={person.id}>
-                  <Box
-                    component="img"
-                    className="personImage"
-                    src={person.image}
-                  />
-                  <Grid className="frame1">
-                    <Typography className="name">{person.name}</Typography>
-                    <Typography className="position">
-                      {person.position}
-                    </Typography>
-                  </Grid>
-                </Grid>
+                <Person key={person.id}>
+                  <img class="personImage" src={person.image} alt="person" />
+                  <Frame1>
+                    <Name>{person.name}</Name>
+                    <Title>{person.position}</Title>
+                  </Frame1>
+                </Person>
               ))}
-            </Grid>
+            </PeopleGrid>
             <ViewButton>View All</ViewButton>
-          </Grid>
+          </Content>
         </SuggestedCard>
         <SuggestedCard2>
           <Grid className="content2">
--- src/modules/knowledge/components/knowledge-view/style.js
+++ src/modules/knowledge/components/knowledge-view/style.js
@@ -5,6 +5,10 @@ import {
   Avatar as Logo,
   Button,
   TextField as MuiText,
+  Typography,
+  Grid,
+  IconButton,
+  Avatar,
 } from "@mui/material";
 import { spacing } from "@mui/system";
 
@@ -32,8 +36,6 @@ Used for Card
 `;
 
 export const AnswerCard1 = styled(MuiGrid)`
-  /* Answer */
-
   /* Auto layout */
 
   display: flex;
@@ -42,8 +44,9 @@ export const AnswerCard1 = styled(MuiGrid)`
   padding: 24px;
   margin: 12px;
 
+  position: relative;
   width: 744px;
-  height: 272px;
+  height: auto;
 
   /* Surface/Default */
 
@@ -64,11 +67,14 @@ export const NewQuestionCard = styled(MuiGrid)`
   display: flex;
   flex-direction: row;
   align-items: center;
+
   padding: 24px;
   margin: 16px;
-  position: relative;
+  align-self: center;
+
   width: 744px;
   height: 84px;
+  position: relative;
 
   /* Surface/Default */
 
@@ -149,17 +155,19 @@ export const SmallAvatar = styled(Logo)`
 `;
 
 export const ViewButton = styled(Button)`
+  box-sizing: border-box;
+
   /* Auto layout */
-  text-transform: none;
+
   display: flex;
   flex-direction: row;
   justify-content: center;
   align-items: center;
-  padding: 5px 12px;
-  margin: 10px;
+  padding: 5px 5px;
 
-  width: 308px;
+  width: 280px;
   height: 30px;
+  text-transform: none;
 
   /* Action Secondary/Default */
 
@@ -176,9 +184,11 @@ export const ViewButton = styled(Button)`
 
   flex: none;
   order: 2;
+  align-self: stretch;
+  flex-grow: 0;
 `;
 
-export const PeopleGrid = styled(MuiCard)`
+export const PeopleGrid = styled(Grid)`
   /* People */
 
   /* Auto layout */
@@ -190,7 +200,7 @@ export const PeopleGrid = styled(MuiCard)`
   margin: 20px;
 
   width: 259px;
-  height: 412px;
+  height: auto;
 
   /* Inside auto layout */
 
@@ -238,4 +248,351 @@ export const SearchField = styled(Text)`
 
   // background: #F1F2F3;
   border-radius: 4px;
+`;
+
+export const ProvidersHeading = styled(Typography)`
+  width: 214px;
+  height: 24px;
+
+  /* Desktop/Heading */
+
+  font-family: "SF Pro Text";
+  font-style: normal;
+  font-weight: 600;
+  font-size: 16px;
+  line-height: 24px;
+  /* identical to box height, or 150% */
+
+  display: flex;
+  align-items: center;
+
+  /* Text/Default */
+
+  color: #202223;
+
+  /* Inside auto layout */
+
+  flex: none;
+  order: 0;
+  flex-grow: 0;
+`;
+
+export const ViewQuestionCard = styled(Typography)`
+  width: 696px;
+  height: 160px;
+
+  /* Desktop/Body */
+
+  font-family: "SF Pro Text";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 20px;
+  /* or 143% */
+
+  /* Text/Default */
+
+  color: #202223;
+
+  /* Inside auto layout */
+
+  flex: none;
+  order: 0;
+  align-self: stretch;
+  flex-grow: 0;
+`;
+export const BottomCard = styled(Grid)`
+  /* Bottom */
+
+  /* Auto layout */
+
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding: 8px 0px;
+  margin: 12px;
+
+  width: 696px;
+  height: 52px;
+
+  /* Inside auto layout */
+
+  flex: none;
+  order: 1;
+  align-self: stretch;
+  flex-grow: 0;
+`;
+
+export const RateCard = styled(Grid)`
+  /* Rate */
+
+  /* Auto layout */
+
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  padding: 0px;
+  margin-right: 5px;
+
+  width: 72px;
+  height: 36px;
+
+  /* Inside auto layout */
+
+  flex: none;
+  order: 0;
+  flex-grow: 0;
+`;
+
+export const RateButton1 = styled(IconButton)`
+  /* Basic button
+
+Used most in the interface. Only use another style if a button requires more or less visual weight.
+*/
+
+  /* Auto layout */
+
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding: 0px;
+  gap: 10px;
+
+  width: 36px;
+  height: 36px;
+
+  border-radius: 4px 0px 0px 4px;
+  border: 1px solid rgba(186, 191, 195, 1);
+  box-shadow: 0px 1px 0px 0px rgba(0, 0, 0, 0.05);
+
+  /* Inside auto layout */
+
+  flex: none;
+  order: 0;
+  flex-grow: 0;
+`;
+
+export const OutLinedButton = styled(Button)`
+  /* Outline button
+
+Used most in the interface. Only use another style if a button requires more or less visual weight.
+*/
+
+  /* Auto layout */
+
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding: 0px;
+  text-transform: none;
+
+  width: 142px;
+  height: 36px;
+
+  background: rgba(255, 255, 255, 0.01);
+  border: 1px solid rgba(255, 255, 255, 0.01);
+  border-radius: 4px;
+
+  /* Inside auto layout */
+
+  flex: none;
+  order: 1;
+  flex-grow: 0;
+`;
+
+export const QuestionIcon = styled(Avatar)`
+  /* chat_major */
+
+  width: 20px;
+  height: 20px;
+
+  /* Inside auto layout */
+
+  flex: none;
+  order: 0;
+  flex-grow: 0;
+`;
+
+export const SourceCard = styled(Grid)`
+  /* Sources */
+
+  /* Auto layout */
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  padding: 0px;
+  margin: 4px;
+
+  width: 696px;
+  height: 68px;
+
+  /* Inside auto layout */
+
+  flex: none;
+  order: 2;
+  align-self: stretch;
+  flex-grow: 0;
+`;
+export const LinkText = styled(Typography)`
+  /* Link */
+
+  width: 696px;
+  height: 20px;
+
+  /* Desktop/Button */
+
+  font-family: "SF Pro Text";
+  font-style: normal;
+  font-weight: 500;
+  font-size: 14px;
+  line-height: 20px;
+  /* identical to box height, or 143% */
+
+  /* Action Primary/Default */
+
+  color: #302b6e;
+
+  /* Inside auto layout */
+
+  flex: none;
+  order: 0;
+  align-self: stretch;
+  flex-grow: 0;
+`;
+
+export const Person = styled(Grid)`
+  /* Person */
+
+  /* Auto layout */
+
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding: 0px;
+  margin: 16px;
+  justify-content: space-between;
+
+  width: 192px;
+  height: 52px;
+
+  /* Inside auto layout */
+
+  flex: none;
+  order: 0;
+  flex-grow: 0;
+`;
+
+export const Frame1 = styled(Grid)`
+  /* Frame 1 */
+
+  /* Auto layout */
+
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: 0px;
+
+  width: 124px;
+  height: 40px;
+
+  /* Inside auto layout */
+
+  flex: none;
+  order: 1;
+  flex-grow: 0;
+`;
+
+export const Content = styled(Grid)`
+  /* Content */
+
+  /* Auto layout */
+
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: 0px;
+  margin: 24px;
+
+  width: 308px;
+  height: 514px;
+
+  /* Inside auto layout */
+
+  flex: none;
+  order: 0;
+  flex-grow: 1;
+`;
+
+export const Name = styled(Typography)`
+Width
+Hug (86px)
+Height
+Hug (24px)
+Robert Fox
+Ag
+Desktop/Heading
+Font
+SF Pro Text
+Weight
+600
+Size
+16px
+Line height
+24px
+Text/Default
+rgba(32, 34, 35, 1)
+/* Name */
+
+
+width: 86px;
+height: 24px;
+
+/* Desktop/Heading */
+
+font-family: 'SF Pro Text';
+font-style: normal;
+font-weight: 600;
+font-size: 16px;
+line-height: 24px;
+/* identical to box height, or 150% */
+
+
+/* Text/Default */
+
+color: #202223;
+
+
+/* Inside auto layout */
+
+flex: none;
+order: 0;
+flex-grow: 0;
+`;
+
+export const Title = styled(Typography)`
+  /* Title */
+
+  width: 124px;
+  height: 16px;
+
+  /* Desktop/Caption */
+
+  font-family: "SF Pro Text";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 12px;
+  line-height: 16px;
+  /* identical to box height, or 133% */
+
+  /* Text/Subdued */
+
+  color: #6d7175;
+
+  /* Inside auto layout */
+
+  flex: none;
+  order: 1;
+  flex-grow: 0;
 `;
